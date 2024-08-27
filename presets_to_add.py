630bf2d75c217ce73a09017d84909c7becd29976
Extraltodeus <extraltodeus@gmail.com>
2024-08-27 18:36:24 +0200

    Update presets_to_add.py

--- presets_to_add.py
+++ presets_to_add.py
@@ -16,11 +16,11 @@ cfgpp will determin if you want it or not. True or False.
 """
 
 extra_samplers["distance_fast"] = distance_wrap(resample=3,resample_end=1,cfgpp=False) # s3e1
-extra_samplers["distance_c2"] = distance_wrap(resample=2,resample_end=-1,cfgpp=False)
 extra_samplers["distance_step"] = distance_wrap(resample=-1,resample_end=1,cfgpp=False)
+extra_samplers["distance_c2"] = distance_wrap(resample=2,resample_end=-1,cfgpp=False)
 
 extra_samplers["distance_fast_cfg_pp"] = distance_wrap(resample=3,resample_end=1,cfgpp=True)
-extra_samplers["distance_c2_cfg_pp"] = distance_wrap(resample=2,resample_end=-1,cfgpp=True)
-extra_samplers["distance_step_cfg_pp"] = distance_wrap(resample=-1,resample_end=1,cfgpp=True)
+# extra_samplers["distance_c2_cfg_pp"] = distance_wrap(resample=2,resample_end=-1,cfgpp=True)
+# extra_samplers["distance_step_cfg_pp"] = distance_wrap(resample=-1,resample_end=1,cfgpp=True)
 
 extra_samplers["heun_cfg_pp"] = distance_wrap(resample=1,cfgpp=True)
